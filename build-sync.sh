b06745fa6f75bd8d270b2d77e45c0ff98edc04a0
Silas S. Brown <ssb22@cam.ac.uk>
2009-10-27 17:54:08 +0000

    Gradint: PocketPC cleanup
    git-svn-id: http://svn.code.sf.net/p/e-guidedog/code/ssb22/gradint@425 29193198-4895-4776-b068-10539e920549

--- build-sync.sh
+++ build-sync.sh
@@ -5,6 +5,6 @@ tar -jxf gradint-build.bgz
 rm -rf gradint/extras # TODO shouldn't be in gradint-build.bgz anyway
 diff -r gradint-build gradint|grep "^Only in gradint-build"|grep -v \\.svn|sed -e 's,Only in ,svn del ",' -e 's,: ,/,' -e 's/$/"/'|bash
 cp -pur gradint/* gradint-build/
-find gradint|grep -v \\.svn|sed -e 's/gradint/gradint-build/' -e 's/^/svn add "' -e 's/$/"/'|bash
+find gradint|grep -v \\.svn|sed -e 's/gradint/gradint-build/' -e 's/^/svn add "/' -e 's/$/"/'|bash
 if test "a$Msg" == a; then export Msg="Gradint update"; fi
 svn commit -m "$Msg"
