1642a37bbd691b991dbd5c71ba846b7ea096d5e0
Silas S. Brown <ssb22@cam.ac.uk>
2009-10-27 17:53:26 +0000

    Gradint: PocketPC cleanup
    git-svn-id: http://svn.code.sf.net/p/e-guidedog/code/ssb22/gradint@424 29193198-4895-4776-b068-10539e920549

--- build-sync.sh
+++ build-sync.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#wget -N http://people.pwf.cam.ac.uk/ssb22/gradint/gradint-build.bgz
-#rm -rf gradint
-#tar -jxf gradint-build.bgz
-#rm -rf gradint/extras # TODO shouldn't be in gradint-build.bgz anyway
-diff -r gradint-build gradint|grep "^Only in gradint-build"|grep -v \\.svn|sed -e 's,Only in ,svn del ,' -e 's,: ,/,'|bash
-#cp -pur gradint/* gradint-build/
-#find gradint-build -exec svn add '{}' ';' # will automatically exclude .svn stuff
-#if test "a$Msg" == a; then export Msg="Gradint update"; fi
+wget -N http://people.pwf.cam.ac.uk/ssb22/gradint/gradint-build.bgz
+rm -rf gradint
+tar -jxf gradint-build.bgz
+rm -rf gradint/extras # TODO shouldn't be in gradint-build.bgz anyway
+diff -r gradint-build gradint|grep "^Only in gradint-build"|grep -v \\.svn|sed -e 's,Only in ,svn del ",' -e 's,: ,/,' -e 's/$/"/'|bash
+cp -pur gradint/* gradint-build/
+find gradint|grep -v \\.svn|sed -e 's/gradint/gradint-build/' -e 's/^/svn add "' -e 's/$/"/'|bash
+if test "a$Msg" == a; then export Msg="Gradint update"; fi
 svn commit -m "$Msg"
