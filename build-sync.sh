41a08464983a1ae0fab1e1579690208ee312ebc0
Silas S. Brown <ssb22@cam.ac.uk>
2009-10-27 17:26:42 +0000

    Gradint
    git-svn-id: http://svn.code.sf.net/p/e-guidedog/code/ssb22/gradint@419 29193198-4895-4776-b068-10539e920549

---
+++ build-sync.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+wget -N http://people.pwf.cam.ac.uk/ssb22/gradint/gradint-build.bgz
+rm -rf gradint
+tar -jxvf gradint-build.bgz
+rm -rf gradint/extras # TODO shouldn't be in gradint-build.bgz anyway
+diff -r gradint-build gradint|grep "^Only in gradint-build"|grep -v \\.svn|sed -e 's,Only in ,svn del ,' -e 's,: ,/,'
+cp -pur gradint/* gradint-build/
+svn add gradint-build
+if test "a$Msg" == a; then export Msg="Gradint update"; fi
+svn commit -m "$Msg"
