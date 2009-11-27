0bb05d892834b38b3b71576224aa8fa08b404fa4
Silas S. Brown <ssb22@cam.ac.uk>
2009-11-27 17:57:47 +0000

    Gradint update
    git-svn-id: http://svn.code.sf.net/p/e-guidedog/code/ssb22/gradint@461 29193198-4895-4776-b068-10539e920549

--- gradint-build/src/system.py
+++ gradint-build/src/system.py
@@ -1,5 +1,5 @@
 # This file is part of the source code of
-# gradint v0.9939 (c) 2002-2009 Silas S. Brown. GPL v3+.
+# gradint v0.99391 (c) 2002-2009 Silas S. Brown. GPL v3+.
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation; either version 3 of the License, or
@@ -285,7 +285,7 @@ if winCEsound and __name__=="__main__":
 Tk_might_display_wrong_hanzi = wrong_hanzi_message = ""
 if macsound:
   if sys.version.startswith("2.3.5"): Tk_might_display_wrong_hanzi="10.4"
-  elif sys.version[:5] >= "2.5.1": # 10.5+
+  elif sys.version[:5] == "2.5.1": # 10.5 (do NOT say >= because this bundle will NOT work on Python 2.6 in OS X 10.6)
     f="/System/Library/Frameworks/Python.framework/Versions/2.5/lib/python2.5/lib-dynload/_tkinter.so"
     if fileExists(f): # we might be able to patch this one up
      if not isDirectory("Frameworks") and fileExists("Frameworks.tbz"): os.system("tar -jxvf Frameworks.tbz && rm Frameworks.tbz && chmod -R +w Frameworks")
@@ -295,8 +295,8 @@ if macsound:
       sys.path.insert(0,os.getcwd()) ; import _tkinter ; del sys.path[0]
       _tkinter.TK_VERSION = _tkinter.TCL_VERSION = "8.6"
     else: Tk_might_display_wrong_hanzi="10.5"
+  elif sys.version[:5] == "2.5.1": Tk_might_display_wrong_hanzi=">10.5 (not tested)" # TODO Test on OS X 10.6 !!!
   if Tk_might_display_wrong_hanzi: wrong_hanzi_message = "NB: In Mac OS "+Tk_might_display_wrong_hanzi+", Chinese\ncan display wrongly here." # so they don't panic when it does
-# TODO can we test on OS X 10.6+ ?  is the above workaround still needed?
 
 # Handle keeping progress file and temp directories etc if we're running from a live CD
 # (and if the live CD has just been copied to the hard disk, look in the old progress file locations also)
