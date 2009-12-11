c5b5377571ae1ba8fc7044bf6d9f5bc9e1286c1c
Silas S. Brown <ssb22@cam.ac.uk>
2009-12-11 17:02:21 +0000

    Gradint update
    git-svn-id: http://svn.code.sf.net/p/e-guidedog/code/ssb22/gradint@473 29193198-4895-4776-b068-10539e920549

--- gradint-build/src/makeevent.py
+++ gradint-build/src/makeevent.py
@@ -1,5 +1,5 @@
 # This file is part of the source code of
-# gradint v0.9942 (c) 2002-2009 Silas S. Brown. GPL v3+.
+# gradint v0.9943 (c) 2002-2009 Silas S. Brown. GPL v3+.
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation; either version 3 of the License, or
@@ -59,7 +59,7 @@ def fileToEvent(fname,dirBase=None):
     if dirBase==None: dirBase=samplesDirectory
     if dirBase: dirBase += os.sep
     orig_fname = fname
-    if os.sep in fname: dirBase,fname = dirBase+fname[:fname.rindex(os.sep)+1], fname[fname.rindex(os.sep)+1:]
+    if os.sep in fname and fname.find("!synth:")==-1: dirBase,fname = dirBase+fname[:fname.rindex(os.sep)+1], fname[fname.rindex(os.sep)+1:]
     if "_" in fname: lang = languageof(fname)
     else: lang="-unknown-" # so can take a simple wav file, e.g. for endAnnouncement
     if dirBase+fname in variantFiles:
