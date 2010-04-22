1b58235a9cc7eb707765324b3d975fe9eeeb488f
Silas S. Brown <ssb22@cam.ac.uk>
2010-04-22 15:19:36 +0000

    Gradint update
    git-svn-id: http://svn.code.sf.net/p/e-guidedog/code/ssb22/gradint@731 29193198-4895-4776-b068-10539e920549

--- build-sync.sh
+++ build-sync.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-wget -N http://people.pwf.cam.ac.uk/ssb22/gradint/gradint-build.bgz
+wget -N http://people.pwf.cam.ac.uk/ssb22/gradint/gradint-build.7z
 rm -rf gradint
-tar -jxf gradint-build.bgz
+7z x gradint-build.7z
 diff -r gradint-build gradint|grep "^Only in gradint-build"|grep -v \\.svn|sed -e 's,Only in ,svn del ",' -e 's,: ,/,' -e 's/$/"/'|bash
 cp -pur gradint/* gradint-build/
 find gradint|grep -v \\.svn|sed -e 's/gradint/gradint-build/' -e 's/^/svn add "/' -e 's/$/"/'|bash
-rm -rf gradint gradint-build.bgz # if need to save space
+rm -rf gradint gradint-build.7z # if need to save space
 if test "a$Msg" == a; then export Msg="Gradint update"; fi
 svn commit -m "$Msg"
