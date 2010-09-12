e52118e00f4828a8a459af99618902f4b50d9a64
Silas S. Brown <ssb22@cam.ac.uk>
2010-09-12 13:39:30 +0000

    Gradint update
    git-svn-id: http://svn.code.sf.net/p/e-guidedog/code/ssb22/gradint@912 29193198-4895-4776-b068-10539e920549

--- gradint-build/src/frontend.py
+++ gradint-build/src/frontend.py
@@ -797,6 +797,7 @@ def startTk():
             self.L1Entry["width"]=self.L2Entry["width"]=3
             self.L1Entry.bind('<Return>',lambda e:e.widget.after(10,lambda e=e:e.widget.event_generate('<Tab>')))
             self.L2Entry.bind('<Return>',self.changeLanguages)
+            for e in [self.L1Entry,self.L2Entry]: e.bind('<Button-1>',(lambda e:e.widget.after(10,lambda e=e:selectAll(e))))
             self.ChangeLanguageButton = addButton(self.row3,"",self.changeLanguages,status="Use this button to set your\nfirst and second languages") # will set text in updateLanguageLabels
             if GUI_omit_settings and (vocabFile==user0[1] or fileExists(vocabFile)): self.row3.pack_forget()
             if (not olpc) or textEditorCommand or explorerCommand: # no point doing this on the XO, because no way to run editor or file browser (unless someone's installed one) and "do it yourself" is probably not helpful in that environment
