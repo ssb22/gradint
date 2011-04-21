8fa4ccd25c597cce1c3160052f3171c6b7bcfde8
Silas S. Brown <ssb22@cam.ac.uk>
2011-04-21 20:26:58 +0000

    Gradint update
    git-svn-id: http://svn.code.sf.net/p/e-guidedog/code/ssb22/gradint@1113 29193198-4895-4776-b068-10539e920549

--- gradint-build/src/makeevent.py
+++ gradint-build/src/makeevent.py
@@ -190,7 +190,8 @@ def synthcache_lookup(fname,dirBase=None,printErrors=0,justQueryCache=0,lang=Non
         global NICcount
         try: NICcount += 1
         except: NICcount=1
-        if NICcount==20: show_info("Further 'not in cache' warnings turned off\n") # (TODO configurable? important on S60 etc)
+        if NICcount>20: pass
+        elif NICcount==20: show_info("Further 'not in cache' warnings turned off\n") # (TODO configurable? important on S60 etc)
         else: show_info("Not in cache: "+r+"\n",True)
 def can_be_synthesized(fname,dirBase=None,lang=None):
     if dirBase==None: dirBase=samplesDirectory
