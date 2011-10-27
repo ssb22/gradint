ff43e72de5d3909c8db1cf36e9ed09dfcc09bb08
Silas S. Brown <ssb22@cam.ac.uk>
2011-10-27 13:32:09 +0000

    Gradint update
    git-svn-id: http://svn.code.sf.net/p/e-guidedog/code/ssb22/gradint@1236 29193198-4895-4776-b068-10539e920549

--- gradint-build/README-Android.txt
+++ gradint-build/README-Android.txt
@@ -11,6 +11,25 @@ On the emulator, you can use e.g.:
 Does not work with very large files, so for partials do not use audiodata.dat
 (use the individual syllable files instead)
 
-TO-DO: Try making a .apk file using the instructions at
-http://code.google.com/p/android-scripting/wiki/SharingScripts
-  but this would complicate the process of releasing new Gradint versions
+------- Making Gradint into a self-contained .apk file -------
+
+This might not work (the device might fail to accept the resulting .apk file)
+so this is only for reference.  Requires an Android SDK installation.
+
+cd /tmp
+wget http://android-scripting.googlecode.com/hg/android/script_for_android_template.zip
+wget http://people.pwf.cam.ac.uk/ssb22/gradint/gradint-android.zip
+mkdir android
+cd android
+unzip ../gradint-android.zip
+cd ..
+mkdir apk
+cd apk
+unzip ../script_for_android_template.zip
+export ANDROID_SDK=~/android-sdk-linux_x86/  # or wherever
+sh configure_package.sh org.ucam.ssb22.gradint
+leafpad build.xml # change project name to "Gradint"
+mv ../android/gradint.py res/raw/script.py 
+mv ../android/advanced.txt ../android/settings.txt ../android/samples ../android/vocab.txt res/raw/
+ant debug
+# now copy bin/Gradint-debug.apk to the device
--- gradint-build/server/lesson-table.py
+++ gradint-build/server/lesson-table.py
@@ -4,7 +4,7 @@
 # for summarizing it to a teacher or native speaker.
 # Reads from progressFile and progressFileBackup.
 
-# Version 1.03 (c) 2011 Silas S. Brown.  License: GPL
+# Version 1.04 (c) 2011 Silas S. Brown.  License: GPL
 
 # Example use:
 # python lesson-table.py | ssh some-server 'mutt -e "set record = \"\";" -e "set charset=\"utf-8\"; set send_charset=\"utf-8\"; set content_type=\"text/html\";" to-address -s "Gradint report"' || echo Send failed
@@ -41,25 +41,52 @@ for tries,l1,l2 in newProg.data:
 del newProg,opd
 changes.sort()
 print '<html><head><meta http_equiv="Content-type" value="text/html; charset=utf-8"><title>Gradint lesson report</title></head><body><h2>Gradint lesson report</h2><table border><tr><th>Repeats before</th><th>Repeats today</th><th>Question</th><th>Answer</th></tr>' # (have Question/Answer order rather than Word/Meaning, because if it's L2-only poetry then the question is the previous line, which is not exactly "meaning")
+  
+had_h5a = False
+def h5aCode(filename):
+  r = real_h5aCode(filename)
+  if r:
+    global had_h5a
+    if not had_h5a:
+      had_h5a = True
+      print """<script language="Javascript"><!--
+function h5a(link,type) { if (document.createElement) {
+   var ae = document.createElement('audio');
+   if (ae.canPlayType && function(s){return s!="" && s!="no"}(ae.canPlayType(type))) {
+     if (link.href) ae.setAttribute('src', link.href);
+     else ae.setAttribute('src', link);
+     ae.play();
+     return false; } } return true; }
+//--></script>"""
+  return r
+def real_h5aCode(filename):
+  if filename.endswith(gradint.dotmp3): return ' onClick="javascript:return h5a(this,\'audio/mpeg\')"'
+  elif filename.endswith(gradint.dotwav): return ' onClick="javascript:return h5a(this,\'audio/wav\')"'
+  else: return ""
+
 def wrappable(f):
   z = u'\u200b'.encode('utf-8') # zero-width space
   return f.replace(os.sep,os.sep+z).replace('_',z+'_')
-def checkVariant(l):
+def checkVariant(l,ensureTxt=0):
   if os.sep in l: fname=l[l.rindex(os.sep)+1:]
   else: fname=l
   variants = gradint.variantFiles.get(gradint.samplesDirectory+os.sep+l,[fname])
   if fname in variants: return l # ok
   # else no default variant, need to pick one for the link
-  if not os.sep in l: return variants[0]
-  return l[:l.rindex(os.sep)+1]+variants[0]
+  for v in variants:
+    if ensureTxt:
+      if not v.endswith(gradint.dottxt): continue
+    elif v.endswith(gradint.dottxt): continue
+    if not os.sep in l: return v
+    return l[:l.rindex(os.sep)+1]+v
 def link(l):
   if type(l)==type([]): return link(l[-1])
-  if l.lower().endswith(gradint.dottxt): l="!synth:"+gradint.u8strip(gradint.read(gradint.samplesDirectory+os.sep+l)).strip(gradint.wsp)+"_"+gradint.languageof(l)
+  if l.lower().endswith(gradint.dottxt): l="!synth:"+gradint.u8strip(gradint.read(gradint.samplesDirectory+os.sep+checkVariant(l,1))).strip(gradint.wsp)+"_"+gradint.languageof(l)
   if "!synth:" in l:
     if gradint.languageof(l) not in [gradint.firstLanguage,gradint.secondLanguage]: l=gradint.textof(l)+" ("+gradint.languageof(l)+")"
     else: l=gradint.textof(l)
     return l.replace('&','&amp;').replace('<','&lt;')
-  if samples_url: return '<A HREF="'+samples_url+checkVariant(l)+'">'+wrappable(l)+'</A>'
+  if samples_url: return '<A HREF="'+samples_url+checkVariant(l)+'"'+h5aCode(checkVariant(l))+'>'+wrappable(l)+'</A>'
   return wrappable(l).replace('&','&amp;').replace('<','&lt;')
 for b4,pos,today,l1,l2 in changes: print '<tr><td>%d</td><td>%d</td><td>%s</td><td>%s</td></tr>' % (b4,today,link(l1),link(l2))
 print '</table></body></html>'
