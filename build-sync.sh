925177fd832f992d9acbe4b4a88edff60cacdb79
Silas S. Brown <ssb22@cam.ac.uk>
2012-09-03 13:53:22 +0000

    Update css-generate.py
    git-svn-id: http://svn.code.sf.net/p/e-guidedog/code/ssb22/gradint@1414 29193198-4895-4776-b068-10539e920549

--- build-sync.sh
+++ build-sync.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Sync gradint build environment to SVN
-wget -N http://people.pwf.cam.ac.uk/ssb22/gradint/gradint-build.7z || exit 1
+# (and Accessibility CSS Generator as well)
+cd css-generator
+wget -N http://people.ds.cam.ac.uk/ssb22/css/css-generate.py
+svn commit -m "Update css-generate.py"
+cd ..
+wget -N http://people.ds.cam.ac.uk/ssb22/gradint/gradint-build.7z || exit 1
 rm -rf gradint
 7z x gradint-build.7z || exit 1
 diff -r gradint-build gradint|grep "^Only in gradint-build"|grep -v \\.svn|sed -e 's,Only in ,svn del ",' -e 's,: ,/,' -e 's/$/"/'|bash
