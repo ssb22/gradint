31fabb6b9af8df56467bf454476cff61ca3d6b25
Silas Brown <ssb22@bbis.us>
2017-11-17 07:28:48 -0600

    build-sync to git

--- build-sync.sh
+++ build-sync.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# Sync gradint build environment to SVN
+# Sync gradint build environment to Git
 wget -N http://people.ds.cam.ac.uk/ssb22/gradint/gradint-build.7z || exit 1
 rm -rf gradint # (any leftover ../gradint/gradint directory)
 7z x gradint-build.7z || exit 1
-diff -r gradint-build gradint|grep "^Only in gradint-build"|grep -v \\.svn|sed -e 's,Only in ,svn del ",' -e 's,: ,/,' -e 's/$/"/'|bash
+diff -r gradint-build gradint|grep "^Only in gradint-build"|grep -v \\.git|sed -e 's,Only in ,git rm ",' -e 's,: ,/,' -e 's/$/"/'|bash
 cp -pur gradint/* gradint-build/
-find gradint|grep -v \\.svn|sed -e 's/gradint/gradint-build/' -e 's/^/svn add "/' -e 's/$/"/'|bash
+find gradint|grep -v \\.git|sed -e 's/gradint/gradint-build/' -e 's/^/git add "/' -e 's/$/"/'|bash
 rm -rf gradint gradint-build.7z # if need to save space
 if test "a$Msg" == a; then export Msg="Gradint update"; fi
-svn commit -m "$Msg"
+git commit -am "$Msg" && git push
