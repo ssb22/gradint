d1dd48e2ea1012a26d6157c863e0d30ce30d810d
Silas S. Brown <ssb22@cam.ac.uk>
2022-05-20 06:54:48 +0100

    player utility Python3 compat

--- gradint-build/samples/utils/player.py
+++ gradint-build/samples/utils/player.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
+# (should work in both Python 2 and Python 3)
 
 # Simple sound-playing server
 # Silas S. Brown - public domain - no warranty
@@ -14,19 +15,27 @@ os.environ["PATH"] += ":/usr/local/bin"
 s=socket.socket()
 s.bind(('',8124))
 s.listen(5)
+if type(b"")==type(""): S=lambda x:x # Python 2
+else: S=lambda x:x.decode("latin1") # Python 3
 while True:
     c,a = s.accept()
-    d = c.recv(4)
-    if d=='RIFF': player = "play - 2>/dev/null" # WAV
+    d = S(c.recv(4))
+    if d=='RIFF': # WAV
+        player = "play - 2>/dev/null"
     elif d=='STOP':
         c.close()
         while not d=='START':
-            c,a = s.accept() ; d = c.recv(5) ; c.close()
+            c,a = s.accept()
+            d = S(c.recv(5)) ; c.close()
         continue
     else: player = "mpg123 - 2>/dev/null" # MP3
     player = os.popen(player,"w")
+    d = d.encode("latin1") # no-op on Python 2
     while d:
-        try: player.write(d)
+        try:
+            try: player.write(d)
+            except TypeError: # Python 3
+                player.buffer.write(d)
         except IOError: break # it was probably killed
         d = c.recv(4096)
     try:
